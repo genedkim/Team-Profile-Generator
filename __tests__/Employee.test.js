8cead351c003b7b2e102abadf055510b96b49740
Gene <genedanielkim@gmail.com>
2023-02-22 16:42:05 -0800

    Finished test for Employee class constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,28 @@
 const Employee = require('../lib/Employee');
+const employee = new Employee('John Doe', 123, 'test@email.com');
 
-test('Testing name', () => {
-    let e = new Employee('Test name');
-    expect(e.name).toBe('Test name');
-})
+describe('Employee', () => {
+    it('should create a new employee object', () => {
+      expect(employee).toEqual({
+        name: 'John Doe',
+        id: 123,
+        email: 'test@email.com'
+      });
+    });
+  
+    it('should return the name of the employee', () => {
+      expect(employee.getName()).toBe('John Doe');
+    });
+  
+    it('should return the ID of the employee', () => {
+      expect(employee.getId()).toBe(123);
+    });
+  
+    it('should return the email of the employee', () => {
+      expect(employee.getEmail()).toBe('test@email.com');
+    });
+  
+    it('should return "Employee" as the role of the employee', () => {
+      expect(employee.getRole()).toBe('Employee');
+    });
+  });
